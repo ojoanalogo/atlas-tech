487003f540f91b179ba035d1ce3e6e5a8dea84d4
Alfonso Reyes <alfonso@molecula.digital>
2026-03-02 11:33:26 -0700

    chore: update astro config with new site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
-  site: "https://atlastech.mx",
+  site: "https://atlas-sinaloa.tech",
   compressHTML: true,
   vite: { plugins: [tailwindcss()] },
 });
